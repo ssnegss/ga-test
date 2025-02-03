ffccfa418cef65875ff52e7bc2ee44549e6b1978
ssnegss <65127070+ssnegss@users.noreply.github.com>
2025-02-03 20:56:37 +0300

    refactor: add type prop to Input

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -3,13 +3,14 @@ import "./input.css";
 type TInputProps = {
    value: string;
    setValue: (value: string) => void;
+   type?: string;
 };
 
-export const Input = ({ value, setValue }: TInputProps) => {
+export const Input = ({ value, setValue, type = "text" }: TInputProps) => {
    return (
       <input
          className="input"
-         type="text"
+         type={type}
          value={value}
          onChange={(e) => setValue(e.target.value)}
       />
